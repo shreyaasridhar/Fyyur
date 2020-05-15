566361e0768901bfbb4e6c69cce1f910c90728a3
shreyaasridhar <shreyanju@gmail.com>
2020-05-15 16:55:38 -0700

    Fixed create added artist and venue search

--- app.py
+++ app.py
@@ -165,27 +165,6 @@ def index():
 def venues():
     # DONE: replace with real venues data.
     #       num_shows should be aggregated based on number of upcoming shows per venue.
-    data = [{
-        "city": "San Francisco",
-        "state": "CA",
-        "venues": [{
-            "id": 1,
-            "name": "The Musical Hop",
-            "num_upcoming_shows": 0,
-        }, {
-            "id": 3,
-            "name": "Park Square Live Music & Coffee",
-            "num_upcoming_shows": 1,
-        }]
-    }, {
-        "city": "New York",
-        "state": "NY",
-        "venues": [{
-            "id": 2,
-            "name": "The Dueling Pianos Bar",
-            "num_upcoming_shows": 0,
-        }]
-    }]
     venue_data = []
     venues = db.session.query(Venue.state, Venue.city, db.func.count(Venue.id).label('count_id')).group_by(Venue.city,Venue.state).all()
     for s,c,i in venues:
@@ -210,6 +189,20 @@ def search_venues():
     # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for Hop should return "The Musical Hop".
     # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+    term = request.form.get('search_term', '')
+    print('%'+term+'%')
+    results = db.session.query(Venue.id,Venue.name,Venue.upcoming_shows_count).filter(Venue.name.ilike('%'+term+'%'))
+    print(results.all())
+    search_data = {}
+    data = []
+    for vid,vname,vshowcount in results:
+        temp = {}
+        temp['id'] = vid
+        temp['name'] = vname
+        temp['num_upcoming_shows'] = vshowcount
+        data.append(temp)
+    search_data['count'] = len(results.all())
+    search_data['data'] = data
     response = {
         "count": 1,
         "data": [{
@@ -218,91 +211,14 @@ def search_venues():
             "num_upcoming_shows": 0,
         }]
     }
-    return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
+    
+    return render_template('pages/search_venues.html', results=search_data, search_term=term)
 
 
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
     # shows the venue page with the given venue_id
     # DONE: replace with real venue data from the venues table, using venue_id
-    data1 = {
-        "id": 1,
-        "name": "The Musical Hop",
-        "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-        "address": "1015 Folsom Street",
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "123-123-1234",
-        "website": "https://www.themusicalhop.com",
-        "facebook_link": "https://www.facebook.com/TheMusicalHop",
-        "seeking_talent": True,
-        "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-        "past_shows": [{
-            "artist_id": 4,
-            "artist_name": "Guns N Petals",
-            "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-            "start_time": "2019-05-21T21:30:00.000Z"
-        }],
-        "upcoming_shows": [],
-        "past_shows_count": 1,
-        "upcoming_shows_count": 0,
-    }
-    data2 = {
-        "id": 2,
-        "name": "The Dueling Pianos Bar",
-        "genres": ["Classical", "R&B", "Hip-Hop"],
-        "address": "335 Delancey Street",
-        "city": "New York",
-        "state": "NY",
-        "phone": "914-003-1132",
-        "website": "https://www.theduelingpianos.com",
-        "facebook_link": "https://www.facebook.com/theduelingpianos",
-        "seeking_talent": False,
-        "image_link": "https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-        "past_shows": [],
-        "upcoming_shows": [],
-        "past_shows_count": 0,
-        "upcoming_shows_count": 0,
-    }
-    data3 = {
-        "id": 3,
-        "name": "Park Square Live Music & Coffee",
-        "genres": ["Rock n Roll", "Jazz", "Classical", "Folk"],
-        "address": "34 Whiskey Moore Ave",
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "415-000-1234",
-        "website": "https://www.parksquarelivemusicandcoffee.com",
-        "facebook_link": "https://www.facebook.com/ParkSquareLiveMusicAndCoffee",
-        "seeking_talent": False,
-        "image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-        "past_shows": [{
-            "artist_id": 5,
-            "artist_name": "Matt Quevedo",
-            "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-            "start_time": "2019-06-15T23:00:00.000Z"
-        }],
-        "upcoming_shows": [{
-            "artist_id": 6,
-            "artist_name": "The Wild Sax Band",
-            "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-            "start_time": "2035-04-01T20:00:00.000Z"
-        }, {
-            "artist_id": 6,
-            "artist_name": "The Wild Sax Band",
-            "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-            "start_time": "2035-04-08T20:00:00.000Z"
-        }, {
-            "artist_id": 6,
-            "artist_name": "The Wild Sax Band",
-            "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-            "start_time": "2035-04-15T20:00:00.000Z"
-        }],
-        "past_shows_count": 1,
-        "upcoming_shows_count": 3,
-    }
-    #data = list(filter(lambda d: d['id'] == venue_id, [data1, data2, data3]))[0]
     return render_template('pages/show_venue.html', venue=Venue.query.get(venue_id))
 
 #  Create Venue
@@ -382,7 +298,21 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
+    term = request.form.get('search_term', '')
+    print('%'+term+'%')
+    results = db.session.query(Artist.id,Artist.name,Artist.upcoming_shows_count).filter(Artist.name.ilike('%'+term+'%'))
+    print(results.all())
+    search_data = {}
+    data = []
+    for aid,aname,ashowcount in results:
+        temp = {}
+        temp['id'] = aid
+        temp['name'] = aname
+        temp['num_upcoming_shows'] = ashowcount
+        data.append(temp)
+    search_data['count'] = len(results.all())
+    search_data['data'] = data
+    # DONE: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
     # search for "band" should return "The Wild Sax Band".
     response = {
@@ -393,7 +323,7 @@ def search_artists():
             "num_upcoming_shows": 0,
         }]
     }
-    return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+    return render_template('pages/search_artists.html', results=search_data, search_term=term)
 
 
 @app.route('/artists/<int:artist_id>')
@@ -618,6 +548,10 @@ def create_artist_form():
 def create_artist_submission():
     # called upon submitting the new artist listing form
     print('Recieved artist', request.form)
+    seeking_venue = False
+    if 'seeking_venue' in request.form:
+        print("Seeking_venue", request.form['seeking_venue'])
+        seeking_venue = request.form['seeking_venue'] == 'y'
     try:
         artist = Artist(name=request.form['name'],
                         city=request.form['city'],
@@ -627,7 +561,7 @@ def create_artist_submission():
                         image_link=request.form['image_link'],
                         website=request.form['website'],
                         facebook_link=request.form['facebook_link'],
-                        seeking_venue=request.form['seeking_venue'],
+                        seeking_venue=seeking_venue,
                         seeking_description=request.form['seeking_description']
                         )
         Artist.insert(artist)
@@ -725,6 +659,7 @@ def create_show_submission():
                       venue_id=request.form['venue_id'],
                       start_time=request.form['start_time']
                       )
+        
         db.session.add(show)
         db.session.commit()
         flash('Show was successfully listed!')
